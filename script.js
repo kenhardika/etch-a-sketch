f34e513bd69afba2908c2ef3dc509d2cec3b78a3
kenhardika <kenzamahardika@gmail.com>
2022-02-08 15:49:05 +0700

    create 16x16 blocks

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-//DOM selector
-const body = document.querySelector('body');
-const div1= document.createElement('div')
-const para = document.createElement('p');
 
 
-//add to the html
-body.append(div1);
-div1.append(para);
-// div2.append(para2);
-// div3.append(para);
-// div4.append(para);
+const canvas = document.querySelector('.canvas');
+const para = document.createElement('a');
+const layer = document.createElement('div');
+// create block as div,
 
-// add class name
-div1.classList.add("col-1");
-body.style.display="grid";
-body.style.gridTemplateColumns = "repeat(4,50px)"
-div1.style.border="solid 2px black";
 
-// div.classList.add("one");
+// repeat it 16 times horizontally in a single row,
+layer.classList.add('layerCanvas');
+layer.style.display="grid";
+layer.style.gridTemplateColumns="repeat(16, 12px)"
+layer.style.gap="0px";
+layer.style.boxSizing="border-box";
+
+for (let j=1; j<=16 ; j++) {
+for (let i=1 ; i<=16; i++) {
+    const div = document.createElement('div');
+    div.style.border = "solid 1px black";
+    div.style.width = "10px";
+    div.style.height = "10px";
+    div.classList.add(`colrow`);
+    div.classList.add(`${i}-${j}`)    
+    layer.append(div);
+};
+}
+
+canvas.append(layer);
+// if they are already 16 blocks in a single row, continue to next row
+// if its the 16th row stop the loop.
